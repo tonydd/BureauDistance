7d8803fbdc2bf2dc01203072ed18c35fc0f8f1be
kermit <thomas.stocker92@gmail.com>
2016-03-15 09:06:39 +0100

    Réagencement des package

--- server/Server.java
+++ server/Server.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rcplib;
+package server;
 
 /**
  *
  * @author Tony
  */
-public class Client {
-    //Sisi la famille
+public class Server {
+    
 }
